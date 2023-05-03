141d51ed75afe1cff65c2b60373302337785cb2b
Hasnathasan <hasnatoooooooo@gmail.com>
2023-05-04 00:16:09 +0600

    404 page added

---
+++ src/components/Error/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h1 className="text-9xl font-bold text-slate-600">404</h1>
+        <h3 className="text-3xl font-semibold text-slate-500">Something Wrong!</h3>
+    </div>
+  );
+};
+
+export default ErrorPage;
